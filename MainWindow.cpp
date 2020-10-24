c71d44ebcefacba6c9e2555f4040b239dd89e20f
Hayden Morgan <haydenmmorgan@gmail.com>
2020-10-24 00:25:52 -0600

    cleanup on reset button functionality

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -70,40 +70,27 @@ void MainWindow::on_horizontalSlider_BallBounciness_valueChanged(int newCoeffici
 
 void MainWindow::on_pushButton_DefaultParameters_clicked()
 {
-    OSGWidget *osgWidget = qobject_cast<OSGWidget *>(findChild<QObject *>("graphicsView"));
-
-    osgWidget->mass = 5.0;
     QSlider *ballMassSlider = qobject_cast<QSlider *>(findChild<QObject *>("horizontalSlider_BallMass"));
     ballMassSlider->setSliderPosition(50);
 
-    osgWidget->radius = 0.5;
-    osgWidget->update_nozzle(0.5);
     QSlider *ballSizeSlider = qobject_cast<QSlider *>(findChild<QObject *>("horizontalSlider_BallSize"));
     ballSizeSlider->setSliderPosition(50);
 
-    osgWidget->coefficientOfRestitution = 0.7;
     QSlider *ballBouncinessSlider = qobject_cast<QSlider *>(findChild<QObject *>("horizontalSlider_BallBounciness"));
-    ballBouncinessSlider->setSliderPosition(50);
+    ballBouncinessSlider->setSliderPosition(70);
 
-    osgWidget->color = 0;
     QSlider *ballColorSlider = qobject_cast<QSlider *>(findChild<QObject *>("horizontalSlider_BallColor"));
     ballColorSlider->setSliderPosition(0);
 
-
-    osgWidget->velocity[2] = 20.0;
     QSlider *ballVelocitySlider = qobject_cast<QSlider *>(findChild<QObject *>("horizontalSlider_BallVelocity"));
     ballVelocitySlider->setSliderPosition(20);
 
-    osgWidget->ballsPerSecond = 5.0;
-    osgWidget->update_ball_update_rate();
     QSlider *ballFrequencySlider = qobject_cast<QSlider *>(findChild<QObject *>("horizontalSlider_BallFrequency"));
     ballFrequencySlider->setSliderPosition(5);
 
-    osgWidget->physics.gravity = -9.81;
     QSlider *gravitySlider = qobject_cast<QSlider *>(findChild<QObject *>("horizontalSlider_Gravity"));
     gravitySlider->setSliderPosition(10);
 
-    osgWidget->physics.fluidDensity = 0.5;
     QSlider *fluidDensitySlider = qobject_cast<QSlider *>(findChild<QObject *>("horizontalSlider_FluidViscosity"));
     fluidDensitySlider->setSliderPosition(5);
 }
